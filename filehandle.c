b68ed94eb0106cda9a6ff970179280c9a0b494f8
Ihavefinished <155361074+blaaddawd@users.noreply.github.com>
2026-06-28 21:36:39 +0700

    digger

--- filehandle.c
+++ filehandle.c
@@ -99,3 +99,10 @@ void saveScore(char arr[], int score){
     }
     fclose(fp);
 }
+int main(){
+    char arr[50];
+    int score;
+    printf("enter: ");
+    scanf("%s %d", arr, &score);
+    saveScore(arr, score );
+}
