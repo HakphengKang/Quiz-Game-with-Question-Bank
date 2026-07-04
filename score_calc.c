4a1610a43eeadc45a97a1c9d1c4e6b187f02261f
soth <pt6026010048@camtech.edu.kh>
2026-07-04 16:40:28 +0700

    done

---
+++ score_calc.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "score_calc.h"
+int calScore(){
+    static int score=0;
+    score++;
+    return score;
+}
+    
+
+
+void displayScore(int currentScore, int totalQuestion){
+
+    if (totalQuestion <= 0){
+        printf("no question answered \n");
+        return;
+    }
+
+    float percentage = (  (float) currentScore  /   totalQuestion) * 100.0f ;
+
+
+
+    printf("Total Questions : %d\n", totalQuestion);
+    printf("Correct Answers : %d\n", currentScore);
+    printf("Accuracy Rate   : %.2f%%\n", percentage);
+
+
+}
+int main(){
+    int bro;
+    int question = 10;
+    calScore();
+    bro = calScore();
+    displayScore(bro,question);
+
+    
+}
+
+
+
+
---
+++ score_calc.h
@@ -0,0 +1,10 @@
+
+#ifndef score_h
+#define score_h
+
+static int cal_score(char userAnswer, char correctAnswer, int currentScore);
+
+static void display_Score(int currentScore, int totalQuestion);
+
+#endif
+
